035e0fa02195084db092663861391ea26d2aec35
tenzin samten <tenzinsamten@tenzins-Air.lan>
2023-02-25 14:24:39 -0500

    update

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var yellowCar = document.getElementById("yellowcar");
 var redCar = document.getElementById("redcar");
+var result = document.getElementById("result");
+var score = document.getElementById("score");
+var game = document.getElementById("game");
+
 
 
 
@@ -7,4 +11,21 @@ yellowCar.addEventListener("animationiteration", function(){
     var random = ((Math.floor(Math.random()*2))*130)
     yellowCar.style.left = random+"px";
 })
+
+
+window.addEventListener("keydown",function(e){
+    if(e.keyCode == "39"){
+        var redCarLeft = parseInt(window.getComputedStyle(redCar).getPropertyValue("left"))
+    if (redCarLeft<130){
+        redCar.style.left = (redCarLeft+130)+"px";
+    }
+    };
+    if(e.keyCode =="37"){
+        var redCarLeft = parseInt(window.getComputedStyle(redCar).getPropertyValue("left"))
+        if(redCarLeft>130){
+            redCar.style.left = (redCarLeft-130)+"px";
+        }
+    }
+})
+
 
