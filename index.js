b13b7caa3ca9b1d74142ecc82566da56a2e66c07
tenzin samten <tenzinsamten@tenzins-Air.lan>
2023-02-25 11:46:36 -0500

    update

--- index.js
+++ index.js
@@ -7,11 +7,4 @@ yellowCar.addEventListener("animationiteration", function(){
     var random = ((Math.floor(Math.random()*2))*130)
     yellowCar.style.left = random+"px";
 })
-redCar.addEventListener("keyup", function(e)){
- if(e.key == "39"){
-    if(redCarleft<260)
-    {
-        redCar.style.left = (redCarLeft+130)+"px"
-    }
- }; s
-}
+
